53118b180b743f086d8f814543881fdd20d5475e
pommering <eduardopommering@hotmail.com>
2022-02-28 18:58:53 -0300

    first commit

---
+++ src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
@@ -0,0 +1,23 @@
+import { ISpecificationsRepository } from "../../repositories/ISpecificationsRepository";
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
+class CreateSpecificationUseCase {
+  constructor(private specificationsRepository: ISpecificationsRepository) {}
+
+  execute({ name, description }: IRequest): void {
+    const specificationAlreadyExist =
+      this.specificationsRepository.findByName(name);
+
+    if (specificationAlreadyExist) {
+      throw new Error("Specification Already exists!");
+    }
+
+    this.specificationsRepository.create({ name, description });
+  }
+}
+
+export { CreateSpecificationUseCase };
---
+++ src/modules/cars/useCases/createSpecification/index.ts
@@ -0,0 +1,13 @@
+import { SpecificationsRepository } from "../../repositories/implementations/SpecificationsRepository";
+import { CreateSpecificationController } from "./CreateSpecificationContoller";
+import { CreateSpecificationUseCase } from "./CreateSpecificationUseCase";
+
+const specificationsRepository = new SpecificationsRepository();
+const createSpecificationUseCase = new CreateSpecificationUseCase(
+  specificationsRepository
+);
+const createSpecificationController = new CreateSpecificationController(
+  createSpecificationUseCase
+);
+
+export { createSpecificationController };

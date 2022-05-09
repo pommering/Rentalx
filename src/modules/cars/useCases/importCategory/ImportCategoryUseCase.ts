b6bbf2ae0d16f6289d2a817b312c2207b2de9985
pommering <eduardopommering@hotmail.com>
2022-05-09 00:18:27 -0300

    rentex

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -30,6 +30,7 @@ class ImportCategoryUseCase {
         })
         .on("end", () => {
           resolve(categories);
+          fs.promises.unlink(file.path);
         })
         .on("error", (err) => {
           reject(err);
